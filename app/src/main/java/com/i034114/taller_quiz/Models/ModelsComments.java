c3f6adcd9a06e7fecc6bbdda1bacb5a55e2a1a11
ivandiaz 2425 <ivandiaz2422@gmail.com>
2017-10-14 20:01:01 -0500

    ivan

--- app/src/main/java/com/i034114/taller_quiz/Models/ModelsComments.java
+++ app/src/main/java/com/i034114/taller_quiz/Models/ModelsComments.java
@@ -5,4 +5,42 @@ package com.i034114.taller_quiz.Models;
  */
 
 public class ModelsComments {
+
+    private int postid;
+    private int id;
+    private String email;
+    private String body;
+
+
+    public int getPostid() {
+        return postid;
+    }
+
+    public void setPostid(int postid) {
+        this.postid = postid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
--- app/src/main/java/com/i034114/taller_quiz/Models/ModelsPost.java
+++ app/src/main/java/com/i034114/taller_quiz/Models/ModelsPost.java
@@ -5,4 +5,42 @@ package com.i034114.taller_quiz.Models;
  */
 
 public class ModelsPost {
+
+    private int userid;
+    private int id;
+    private String title;
+    private String body;
+
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
